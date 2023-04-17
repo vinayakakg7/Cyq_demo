ecbd5a08eb12dd32be42e3e9513396226ec5c2a3
vkg <vkg@gmail.com>
2023-04-17 12:33:24 +0530

    env defined

--- automation.tf
+++ automation.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   shared_credentials_file = "/home/ansible/.aws/credentials"
-  shared_config_file = "/home/ansible/.aws/config"
+  shared_config_files = "/home/ansible/.aws/config"
   profile = "aws"
 }
 
